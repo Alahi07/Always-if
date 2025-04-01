f50391d793464ed4ce788491fdbc7f42f28876ec
Aname Alahi <anamealahi07@gmail.com>
2025-04-01 19:32:39 +0400

    Update design.sv

--- design.sv
+++ design.sv
@@ -17,3 +17,23 @@ module top_module(
     end
 
 endmodule
+// synthesis verilog_input_version verilog_2001
+module top_module(
+    input a,
+    input b,
+    input sel_b1,
+    input sel_b2,
+    output wire out_assign,
+    output reg out_always   ); 
+    // Using assign statement
+    assign out_assign = (sel_b1 && sel_b2) ? b : a;
+    
+    // Using procedural if statement
+    always @(*) begin
+        if (sel_b1 && sel_b2)
+            out_always = b;
+        else
+            out_always = a;
+    end
+
+endmodule
